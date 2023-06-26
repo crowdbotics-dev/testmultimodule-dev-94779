620432591ba96b4473dd6d5ffd5d8d64de60e53d
Crowdbotics <engineering@crowdbotics.com>
2023-06-26 16:27:08 +0000

    Update store.

--- store/testmultimoduledevAPI/api.js
+++ store/testmultimoduledevAPI/api.js
@@ -1,76 +1,74 @@
-import axios from "axios";
+import axios from "axios"
 const testmultimoduledevAPI = axios.create({
   baseURL: "https://testmultimodule-dev-94779.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return testmultimoduledevAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return testmultimoduledevAPI.post(`/api/v1/login/`, payload.data);
+  return testmultimoduledevAPI.post(`/api/v1/login/`, payload.data)
 }
-
 function api_v1_signup_create(payload) {
-  return testmultimoduledevAPI.post(`/api/v1/signup/`, payload.data);
+  return testmultimoduledevAPI.post(`/api/v1/signup/`, payload.data)
 }
-
+function modules_camera_photos_user_list(payload) {
+  return testmultimoduledevAPI.get(`/modules/camera/photos/user/`)
+}
+function modules_camera_photos_user_retrieve(payload) {
+  return testmultimoduledevAPI.get(`/modules/camera/photos/user/${payload.id}/`)
+}
+function modules_camera_upload_image_create(payload) {
+  return testmultimoduledevAPI.post(`/modules/camera/upload_image/`)
+}
 function rest_auth_login_create(payload) {
-  return testmultimoduledevAPI.post(`/rest-auth/login/`, payload.data);
+  return testmultimoduledevAPI.post(`/rest-auth/login/`, payload.data)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return testmultimoduledevAPI.get(`/rest-auth/logout/`);
+  return testmultimoduledevAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return testmultimoduledevAPI.post(`/rest-auth/logout/`);
+  return testmultimoduledevAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return testmultimoduledevAPI.post(`/rest-auth/password/change/`, payload.data);
+  return testmultimoduledevAPI.post(`/rest-auth/password/change/`, payload.data)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return testmultimoduledevAPI.post(`/rest-auth/password/reset/`, payload.data);
+  return testmultimoduledevAPI.post(`/rest-auth/password/reset/`, payload.data)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return testmultimoduledevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
+  return testmultimoduledevAPI.post(
+    `/rest-auth/password/reset/confirm/`,
+    payload.data
+  )
 }
-
 function rest_auth_registration_create(payload) {
-  return testmultimoduledevAPI.post(`/rest-auth/registration/`, payload.data);
+  return testmultimoduledevAPI.post(`/rest-auth/registration/`, payload.data)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return testmultimoduledevAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
+  return testmultimoduledevAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    payload.data
+  )
 }
-
 function rest_auth_user_retrieve(payload) {
-  return testmultimoduledevAPI.get(`/rest-auth/user/`);
+  return testmultimoduledevAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return testmultimoduledevAPI.put(`/rest-auth/user/`, payload.data);
+  return testmultimoduledevAPI.put(`/rest-auth/user/`, payload.data)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return testmultimoduledevAPI.patch(`/rest-auth/user/`, payload.data);
+  return testmultimoduledevAPI.patch(`/rest-auth/user/`, payload.data)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  modules_camera_photos_user_list,
+  modules_camera_photos_user_retrieve,
+  modules_camera_upload_image_create,
   rest_auth_login_create,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
@@ -82,4 +80,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
