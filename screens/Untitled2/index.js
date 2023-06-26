17b403f668d82169afcf028fe43fc6e65136ad55
Kushal Karki <kushal@crowdbotics.com>
2023-06-26 14:37:17 +0545

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled2/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+  import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+  const Untitled2 = () => {
+    return (
+      <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+          backgroundColor: '#f0f0f1',
+          padding: 10,
+          position: 'relative',
+          flex: 1
+        }}></ScrollView>
+    </SafeAreaView>
+    )};
+
+    const styles = StyleSheet.create({
+      safeArea: {
+        height:'100%'
+      },
+    });
+
+  export default Untitled2;
